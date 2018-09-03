f081ef831ab8ea7ef8c2bc17c6cfaad4eee3aa31
nataliapalomares <npalomares@pucp.pe>
2018-09-03 15:16:02 -0500

    Ventana de carga de datos (borrador)

--- src/optimizarcargahornos/OptimizarCargaHornos.java
+++ src/optimizarcargahornos/OptimizarCargaHornos.java
@@ -14,20 +14,28 @@ public class OptimizarCargaHornos {
     /**
      * @param args the command line arguments
      */
-    private Solucion GreedyRandomizedAlgorithm(){
+    private Solucion ConstruirUnaSolucion(Horno oven, Pedido[] listaPedidos){
+        Solucion sol=new Solucion(oven);
+        
         return null;
     }
     private boolean valido(Solucion solucion){
         return true;
     }
+    private Pedido[] procesarPedidos(){
+        return null;
+    }
     public void main(String[] args) {
+        //procesarPedidos
+        Pedido[] listaPedidos=new Pedido[20];
+        listaPedidos=procesarPedidos();
         //INICIO DE GRASP
         int numIteraciones = 150; //CALIBRAR VALOR
         Horno oven=new Horno(1000,2000,10, 3); //Inicialización del horno
         List<Solucion> poblacionInicial=new ArrayList<Solucion>();
         for(int k=0;k<numIteraciones;k++){
             Solucion solIteracion=new Solucion(oven);
-            solIteracion=GreedyRandomizedAlgorithm();
+         //   solIteracion=ConstruirUnaSolucion(oven);
             if(valido(solIteracion)){
                 poblacionInicial.add(solIteracion);
             }else{
